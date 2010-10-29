26bbaa01a343b0ccfc39503514495237563b7cff
andreas.juffinger@kb.nl <andreas.juffinger@kb.nl>
2010-10-29 09:24:15 +0000

    refactored default api implementation into separat plugin

--- api/src/test/java/eu/europeana/uim/UIMRegistryTest.java
+++ api/src/test/java/eu/europeana/uim/UIMRegistryTest.java
@@ -1,22 +1,16 @@
 package eu.europeana.uim;
 
-import eu.europeana.uim.api.Registry;
-import eu.europeana.uim.store.memory.MemoryStorageEngine;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/META-INF/spring/test-bundle-context.xml")
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.europeana.uim.api.Registry;
+import eu.europeana.uim.store.memory.MemoryStorageEngine;
+
 public class UIMRegistryTest {
 
-    @Autowired
-    private Registry registry;
+    private Registry registry = new UIMRegistry();
 
 	@Before
 	public void setup() {
