b5eeb8351564e1cd83e88c4f998b8f5e89031d33
andreas.juffinger@kb.nl <andreas.juffinger@kb.nl>
2010-12-11 00:02:51 +0000

    added serialized id to exception

--- api/src/main/java/eu/europeana/uim/api/StorageEngineException.java
+++ api/src/main/java/eu/europeana/uim/api/StorageEngineException.java
@@ -2,6 +2,10 @@ package eu.europeana.uim.api;
 
 public class StorageEngineException extends Exception {
 
+	/**
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public StorageEngineException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 	}
