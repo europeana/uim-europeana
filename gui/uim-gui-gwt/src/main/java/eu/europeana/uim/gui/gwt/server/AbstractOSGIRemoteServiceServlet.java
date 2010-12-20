b3aaea71abac4a879e0473d9eae70f70d6d7d273
manuel.gay@ait.ac.at <manuel.gay@ait.ac.at>
2010-12-20 15:22:09 +0000

    - adjusting some import statements - setting maven-bundle-plugin version to 2.1.0 - more integrated, generic way of launching the GWT development mode

---
+++ gui/uim-gui-gwt/src/main/java/eu/europeana/uim/gui/gwt/server/AbstractOSGIRemoteServiceServlet.java
@@ -0,0 +1,34 @@
+package eu.europeana.uim.gui.gwt.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * RemoteServiceServlet that is integrated with the UIM OSGI platform.
+ *
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+public class AbstractOSGIRemoteServiceServlet extends RemoteServiceServlet {
+
+    private static final String DEVMODE = "devmode";
+
+    protected final UIMEngine engine;
+
+    public AbstractOSGIRemoteServiceServlet() {
+        this.engine = UIMDependenciesActivator.getUIMEngine();
+    }
+
+    @Override
+    protected void doUnexpectedFailure(Throwable e) {
+        e.printStackTrace();
+    }
+
+    @Override
+    protected void checkPermutationStrongName() throws SecurityException {
+        // this is defined in the gwt:run mojo of the POM
+        // without it, GWT won't let us run against the deployed version on the OSGI platform
+        if(System.getenv(DEVMODE) != null && System.getenv(DEVMODE).equals("true")) {
+            return;
+        }
+        super.checkPermutationStrongName();
+    }
+}
--- gui/uim-gui-gwt/src/main/java/eu/europeana/uim/gui/gwt/server/WorkflowServiceImpl.java
+++ gui/uim-gui-gwt/src/main/java/eu/europeana/uim/gui/gwt/server/WorkflowServiceImpl.java
@@ -1,7 +1,5 @@
 package eu.europeana.uim.gui.gwt.server;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import eu.europeana.uim.api.Registry;
 import eu.europeana.uim.gui.gwt.client.WorkflowService;
 import eu.europeana.uim.gui.gwt.shared.Workflow;
 
@@ -11,28 +9,22 @@ import java.util.List;
 /**
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
-public class WorkflowServiceImpl extends RemoteServiceServlet implements WorkflowService {
-
-    private final Registry registry;
+public class WorkflowServiceImpl extends AbstractOSGIRemoteServiceServlet implements WorkflowService {
 
     public WorkflowServiceImpl() {
-        this.registry = RemoteServiceDependenciesActivator.getRegistry();
+        super();
     }
 
     @Override
     public List<Workflow> getWorkflows() {
-        System.out.println("I got a freaking registry: " + registry);
+        System.out.println("I got a freaking engine: " + engine);
 
         List<Workflow> res = new ArrayList<Workflow>();
-        List<eu.europeana.uim.api.Workflow> workflows = registry.getWorkflows();
+        List<eu.europeana.uim.api.Workflow> workflows = engine.getRegistry().getWorkflows();
         for(eu.europeana.uim.api.Workflow w : workflows) {
             res.add(new Workflow(w.getName(), w.getDescription()));
         }
         return res;
     }
 
-    @Override
-    protected void doUnexpectedFailure(Throwable e) {
-        e.printStackTrace();
-    }
 }
