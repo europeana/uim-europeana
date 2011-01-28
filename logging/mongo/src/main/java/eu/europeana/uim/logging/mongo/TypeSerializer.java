6b17f87e724e2a1659703b24407567026ab76fd5
manuel.gay@ait.ac.at <manuel.gay@ait.ac.at>
2011-01-28 10:52:02 +0000

    generic mongodb-based logging engine, can be extended to fit specific cases

---
+++ logging/mongo/src/main/java/eu/europeana/uim/logging/mongo/TypeSerializer.java
@@ -0,0 +1,26 @@
+package eu.europeana.uim.logging.mongo;
+
+import com.mongodb.DBObject;
+
+/**
+ * This interface defines the contract necessary to persist an arbitrary entity with the mongo based logging engine
+ *
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+public interface TypeSerializer<T> {
+
+    /**
+     * Serializes a type into a DBObject
+     *
+     * @param type the object to serialize
+     * @return a DBObject
+     */
+    DBObject serialize(T type);
+
+    /**
+     * De-serializes an object from the mongo storage
+     * @param object the DBObject to dehydrate
+     * @return a Typed object
+     */
+    T parse(DBObject object);
+}
