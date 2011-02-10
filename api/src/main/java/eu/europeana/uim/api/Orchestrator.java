6e4dde7ea34c44e85ff0064b269b9fc610f94439
manuel.gay@ait.ac.at <manuel.gay@ait.ac.at>
2011-02-10 19:36:16 +0000

    Cleaning up

--- api/src/main/java/eu/europeana/uim/api/Orchestrator.java
+++ api/src/main/java/eu/europeana/uim/api/Orchestrator.java
@@ -9,34 +9,15 @@ import eu.europeana.uim.store.DataSet;
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 public interface Orchestrator {
-	
-	public String getIdentifier();
+
+    public String getIdentifier();
 
     ActiveExecution<?> executeWorkflow(Workflow w, DataSet dataset, ProgressMonitor monitor);
 
-
     <T> ActiveExecution<T> getActiveExecution(long id);
+
     <T> java.util.Collection<ActiveExecution<T>> getActiveExecutions();
 
-
     void shutdown();
-
-    /**
-     * Gets the next batch of MetaDataRecord IDs for a given Execution
-     * @param e the Execution for which to retrieve the next batch of IDs
-     * @return an array of MetaDataRecord IDs to process
-     */
-    //long[] getBatchFor(ActiveExecution e);
-
-    /**
-     * Gets the total number of items to be processed for this dataset
-     */
-    //int getTotal(ActiveExecution dataset);
-
-    /**
-     * Gets a snapshot of the runtime information for a given workflow.
-     * As we run multiple executions through the same WorkflowProcessor, this is a global status, not related to a particular execution.
-     */
-    //List<WorkflowStepStatus> getRuntimeStatus(Workflow w);
 
 }
