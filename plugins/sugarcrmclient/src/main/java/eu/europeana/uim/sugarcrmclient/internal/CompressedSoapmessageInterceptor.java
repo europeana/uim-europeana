70b661f51c28e7e72c4d8b0f3626e2bd9e140b82
georgios.markakis@kb.nl <georgios.markakis@kb.nl>
2011-02-23 09:00:26 +0000

    Added customized Spring message interceptor & soap message factory

---
+++ plugins/sugarcrmclient/src/main/java/eu/europeana/uim/sugarcrmclient/internal/CompressedSoapmessageInterceptor.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package eu.europeana.uim.sugarcrmclient.internal;
+
+import org.springframework.ws.client.WebServiceClientException;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.context.MessageContext;
+import org.springframework.beans.factory.InitializingBean;
+/**
+ * @author geomark
+ *
+ */
+public class CompressedSoapmessageInterceptor implements ClientInterceptor,InitializingBean {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.ws.client.support.interceptor.ClientInterceptor#handleFault(org.springframework.ws.context.MessageContext)
+	 */
+	@Override
+	public boolean handleFault(MessageContext arg0)
+			throws WebServiceClientException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.ws.client.support.interceptor.ClientInterceptor#handleRequest(org.springframework.ws.context.MessageContext)
+	 */
+	@Override
+	public boolean handleRequest(MessageContext arg0)
+			throws WebServiceClientException {
+
+		
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.ws.client.support.interceptor.ClientInterceptor#handleResponse(org.springframework.ws.context.MessageContext)
+	 */
+	@Override
+	public boolean handleResponse(MessageContext arg0)
+			throws WebServiceClientException {
+
+		return true;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
