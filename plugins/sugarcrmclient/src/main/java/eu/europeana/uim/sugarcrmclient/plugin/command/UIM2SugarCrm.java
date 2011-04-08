4be92f4f8078b3c3ae72481fe457d38ed3a29c68
georgios.markakis@kb.nl <georgios.markakis@kb.nl>
2011-04-08 11:06:30 +0000

    Made compatible with the 2.2.0 karaf version

--- plugins/sugarcrmclient/src/main/java/eu/europeana/uim/sugarcrmclient/plugin/command/UIM2SugarCrm.java
+++ plugins/sugarcrmclient/src/main/java/eu/europeana/uim/sugarcrmclient/plugin/command/UIM2SugarCrm.java
@@ -9,8 +9,8 @@ import org.apache.felix.gogo.commands.Action;
 import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.felix.gogo.commands.Option;
-import org.osgi.service.command.CommandSession;
-import org.osgi.service.command.Function;
+import org.apache.felix.service.command.CommandSession;
+import org.apache.felix.service.command.Function;
 
 import eu.europeana.uim.sugarcrmclient.plugin.SugarCRMAgent;
 import eu.europeana.uim.sugarcrmclient.plugin.SugarCRMAgentImpl;
