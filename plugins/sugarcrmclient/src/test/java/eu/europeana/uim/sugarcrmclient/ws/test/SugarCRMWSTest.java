b1e9bb02ed233a74e5d6f25461e9eddbb67be733
georgios.markakis@kb.nl <georgios.markakis@kb.nl>
2011-04-14 13:23:50 +0000

    Javadoc comments

--- plugins/sugarcrmclient/src/test/java/eu/europeana/uim/sugarcrmclient/ws/test/SugarCRMWSTest.java
+++ plugins/sugarcrmclient/src/test/java/eu/europeana/uim/sugarcrmclient/ws/test/SugarCRMWSTest.java
@@ -1,15 +1,28 @@
-/**
- * 
+/*
+ * Copyright 2007 EDL FOUNDATION
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * you may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
  */
 package eu.europeana.uim.sugarcrmclient.ws.test;
 
 import static org.junit.Assert.*;
-
 import javax.annotation.Resource;
-
-
 import java.util.ArrayList;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,10 +30,8 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 import eu.europeana.uim.sugarcrmclient.ws.SugarWsClient;
 import eu.europeana.uim.sugarcrmclient.ws.exceptions.LoginFailureException;
-
 
 import eu.europeana.uim.sugarcrmclient.jibxbindings.GetAvailableModules;
 import eu.europeana.uim.sugarcrmclient.jibxbindings.GetAvailableModulesResponse;
