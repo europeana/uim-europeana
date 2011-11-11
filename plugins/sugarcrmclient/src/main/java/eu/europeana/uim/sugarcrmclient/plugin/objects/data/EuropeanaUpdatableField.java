481b65d35860387aa1da702e3d3470a13b2e6aa1
yorgos.mamakis@kb.nl <yorgos.mamakis@kb.nl>
2011-11-11 16:27:47 +0000

    Updated Enumeration with new Updatable fields from SugarCRM

--- plugins/sugarcrmclient/src/main/java/eu/europeana/uim/sugarcrmclient/plugin/objects/data/EuropeanaUpdatableField.java
+++ plugins/sugarcrmclient/src/main/java/eu/europeana/uim/sugarcrmclient/plugin/objects/data/EuropeanaUpdatableField.java
@@ -28,6 +28,7 @@ import eu.europeana.uim.sugarcrm.model.UpdatableField;
  *  by the SugarCRM client. 
  *  
  * @author Georgios Markakis
+ * @author Yorgos Mamakis
  */
 public enum EuropeanaUpdatableField implements UpdatableField{
 	AMOUNT("amount","amount","Amount"),
@@ -36,6 +37,26 @@ public enum EuropeanaUpdatableField implements UpdatableField{
 	INGESTED_VIDEO("ingested_video_c","ingested_video_c","Ingested Video"),
 	INGESTED_TEXT("ingested_text_c","ingested_text_c","Ingested Text"),
 	INGESTED_IMAGE("ingested_image_c","ingested_image_c","Ingested Images"),
+	ADDRESS("address_c", "address_c", "Address"),
+	PORT("port_c","port_c", "Port"),
+	DATABASE("database_c","database_c", "Database"),
+	USER("user_c","user_c","User"),
+	PASSWORD("password_c","password_c", "Password"),
+	RECORD_SYNTAX("record_syntax_c","record_syntax_c", "Record Syntax"),
+	CHARSET("charset_c","charset_c", "Charset"),
+	Z3950METHOD("z39_50_method_c","z39_50_method_c", "Z3950 Method"),
+	RECORD_ID_POLICY("record_id_policy_c","record_id_policy_c", "Record ID Policy"),
+	NAMESPACE_PREFIX("namespace_prefix_c", "namespace_prefix_c", "Namespace Prefix"),
+	NAMESPACE_URI("namespace_uri_c", "namespace_uri_c", "Namespace URI"),
+	IDXPATH("idxpath_c","idxpath_c", "IdXPath"),
+	FILEPATH("filepath_c","filepath_c","File Path"),
+	MAXIMUMID("maximumid_c", "maximumid_c", "Maximum ID"),
+	EARLIEST_TIMESTAMP("earliest_timestamp_c","earliest_timestamp_c", "Earliest Timestamp"),
+	FTPPATH("ftppath_c","ftppath_c", "FTP Path"),
+	ISOFORMAT("isoformat_c","isoformat_c", "ISO Format"),
+	SERVER("server_c","server_c", "Server"),
+	HTTPURL("http_url_c","http_url_c", "HTTP URL"),
+	FOLDER("folder_c","folder_c","Folder"),
 	NEXT_STEP("next_step","next_step","Next Step"),
 	STATUS("sales_stage","sales_stage","sales_stage"),
 	TYPE("opportunity_type","opportunity_type","Type"),
