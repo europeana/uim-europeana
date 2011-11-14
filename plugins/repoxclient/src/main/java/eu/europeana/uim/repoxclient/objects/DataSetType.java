a28c7db0f213fce254bb9cde5580a636e0301b7b
georgios.markakis@kb.nl <georgios.markakis@kb.nl>
2011-11-14 15:33:41 +0000

    Data Set Types

--- plugins/repoxclient/src/main/java/eu/europeana/uim/repoxclient/objects/DataSetType.java
+++ plugins/repoxclient/src/main/java/eu/europeana/uim/repoxclient/objects/DataSetType.java
@@ -1,5 +1,20 @@
 package eu.europeana.uim.repoxclient.objects;
 
-public enum DataSetType {
+/**
+ * Repox DataSet Types (Provider Attributes)
+ * 
+ * @author Georgios Markakis
+ */
 
+public enum DataSetType {
+	MUSEUM,
+	ARCHIVE,
+	LIBRARY,
+	AUDIO_VISUAL_ARCHIVE,
+	RESEARCH_EDUCATIONAL,
+	CROSS_SECTOR,
+	PUBLISHER,
+	PRIVATE,
+	AGGREGATOR,
+	UNKNOWN
 }
