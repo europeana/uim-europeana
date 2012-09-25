1f329ce72d32e93dc88082549eea47d98d6c3338
georgios.markakis@kb.nl <georgios.markakis@kb.nl>
2012-09-25 16:48:40 +0000

    Code formatting & minor corrections

--- webapp/controlpanel/src/main/java/eu/europeana/uim/gui/cp/server/MongoImageViewServlet.java
+++ webapp/controlpanel/src/main/java/eu/europeana/uim/gui/cp/server/MongoImageViewServlet.java
@@ -45,144 +45,164 @@ import eu.europeana.uim.model.europeana.EuropeanaModelRegistry;
 import eu.europeana.uim.store.MetaDataRecord;
 
 /**
- * Servlet that communicates with the underlying Mongo Image 
- * Storage Repository 
+ * Servlet that communicates with the underlying Mongo Image Storage Repository
  * 
  * @author Georgios Markakis <gwarkx@hotmail.com>
- *
+ * 
  * @since 27 Jul 2012
  */
 public class MongoImageViewServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-    private  ExpandedOsgiEngine        engine;
+	private ExpandedOsgiEngine engine;
 	private ThumbSize size;
 	private static ThumbnailService thumbnailHandler;
-	
-    /* (non-Javadoc)
-     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-     */
-    @SuppressWarnings("restriction")
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest
+	 * , javax.servlet.http.HttpServletResponse)
+	 */
+	@SuppressWarnings("restriction")
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    	
-		 PrintWriter out = response.getWriter();
-		 response.setContentType("text/html");
-		 
-    	String recid = request.getParameter("recordID");
-    	 
-    	@SuppressWarnings("unchecked")
-		StorageEngine<String> storagenegine = (StorageEngine<String>) engine.getRegistry().getStorageEngine();
-    	
-    	 try {
-			MetaDataRecord<String> record = storagenegine.getMetaDataRecord(recid);
-			
-			List<EuropeanaLink> values = record.getValues(EuropeanaModelRegistry.EUROPEANALINK);
-			
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+
+		String recid = request.getParameter("recordID");
+
+		@SuppressWarnings("unchecked")
+		StorageEngine<String> storagenegine = (StorageEngine<String>) engine
+				.getRegistry().getStorageEngine();
+
+		try {
+			MetaDataRecord<String> record = storagenegine
+					.getMetaDataRecord(recid);
+
+			List<EuropeanaLink> values = record
+					.getValues(EuropeanaModelRegistry.EUROPEANALINK);
+
 			out.write("<h1>Cached Images for record" + recid + "</h1>");
-			
-			for(EuropeanaLink value:values){
-			
-				String url = value.getUrl();
 
-			ImageCache imgcache = thumbnailHandler.findByOriginalUrl(url);
-			
-			if(imgcache == null){
-				out.write("<h2>No images were cached for link with id" + url+ "</h2>");
-				
-			}
-			else
-			{
-			
-				out.write("<h2> Cached images for link " + url+ "</h2>");
-			 Map<String, Image> imgs = imgcache.getImages();
-			 
-			 Image big = imgs.get("LARGE");
-			 
-			 Image small = imgs.get("MEDIUM");
-			 
-			 Image tiny = imgs.get("TINY");
-			 
-			 String bigbase64 =  new sun.misc.BASE64Encoder().encode(big.getImage());
-			 String smallbase64 =  new sun.misc.BASE64Encoder().encode(small.getImage());
-			 String tinybase64 =  new sun.misc.BASE64Encoder().encode(tiny.getImage());
+			for (EuropeanaLink value : values) {
+				if (value.isCacheable()) {
 
-			 String bigxmpData = thumbnailHandler.extractXMPInfo(imgcache.getObjectId(), imgcache.getImageId(), ThumbSize.LARGE);
-			 String mediumxmpData = thumbnailHandler.extractXMPInfo(imgcache.getObjectId(), imgcache.getImageId(), ThumbSize.MEDIUM);
-			 String smallxmpData =  thumbnailHandler.extractXMPInfo(imgcache.getObjectId(), imgcache.getImageId(), ThumbSize.TINY);
-			 
-			 bigxmpData = bigxmpData.replaceAll("<","&lt;").replaceAll(">","&gt;");
-			 mediumxmpData = mediumxmpData.replaceAll("<","&lt;").replaceAll(">","&gt;");
-			 smallxmpData = smallxmpData.replaceAll("<","&lt;").replaceAll(">","&gt;");
-			 
-			 out.write("</hr>"); 
-			 out.write("<table border=\"1\">");
-			 out.write("<tr><th><b>Thumbnail</b></th><th><b>Embedded XMP Metadata</b></th></tr>"); 
-			 out.write("<tr>");
-			 out.write("<td>");
-			 out.write("<h3>Large thumbnail: </h3>");
-			 out.write("<img src=\"data:image/jpeg;base64," + bigbase64 + "\" />");
-			 out.write("</td>");
-			 out.write("<td>");
-			 
-			 if(bigxmpData != null){
-				 out.write("<pre>");
-				 out.write(bigxmpData);
-				 out.write("</pre>");
-			 }
-			 else{
-				 out.write("<b>NO METADATA</b>");
-			 }
-			 out.write("</td>");
-			 out.write("</tr>");
-			 out.write("<tr>");
-			 out.write("<td>");
-			 out.write("<h3>Medium thumbnail: </h3>");
-			 out.write("<img src=\"data:image/jpeg;base64," + smallbase64 + "\" />");
-			 out.write("</td>");
-			 out.write("<td>");
-			 if(mediumxmpData != null){
-				 out.write("<pre>");
-				 out.write(mediumxmpData);
-				 out.write("</pre>");
-			 }
-			 else{
-				 out.write("<b>NO METADATA</b>");
-			 }
-			 out.write("</td>");
-			 out.write("</tr>");
-			 out.write("<tr>");
-			 out.write("<td>");
-			 out.write("<h3>Tiny thumbnail: </h3>");
-			 out.write("<img src=\"data:image/jpeg;base64," + tinybase64 + "\" />");
-			 out.write("</td>");
-			 out.write("<td>");
-			 if(smallxmpData != null){
-				 out.write("<pre>");
-				 out.write(smallxmpData);
-				 out.write("</pre>");
-			 }
-			 else{
-				 out.write("<b>NO METADATA</b>");
-			 }
-			 out.write("</td>");
-			 out.write("</tr>");
-			 
-			 out.write("</table>");
-			 
+					String url = value.getUrl();
+
+					ImageCache imgcache = thumbnailHandler
+							.findByOriginalUrl(url);
+
+					if (imgcache == null) {
+						out.write("<h2>No images were cached for link with id"
+								+ url + "</h2>");
+
+					} else {
+
+						out.write("<h2> Cached images for link " + url
+								+ "</h2>");
+						Map<String, Image> imgs = imgcache.getImages();
+
+						Image big = imgs.get("LARGE");
+
+						Image small = imgs.get("MEDIUM");
+
+						Image tiny = imgs.get("TINY");
+
+						String bigbase64 = new sun.misc.BASE64Encoder()
+								.encode(big.getImage());
+						String smallbase64 = new sun.misc.BASE64Encoder()
+								.encode(small.getImage());
+						String tinybase64 = new sun.misc.BASE64Encoder()
+								.encode(tiny.getImage());
+
+						String bigxmpData = thumbnailHandler.extractXMPInfo(
+								imgcache.getObjectId(), imgcache.getImageId(),
+								ThumbSize.LARGE);
+						String mediumxmpData = thumbnailHandler.extractXMPInfo(
+								imgcache.getObjectId(), imgcache.getImageId(),
+								ThumbSize.MEDIUM);
+						String smallxmpData = thumbnailHandler.extractXMPInfo(
+								imgcache.getObjectId(), imgcache.getImageId(),
+								ThumbSize.TINY);
+
+						bigxmpData = bigxmpData.replaceAll("<", "&lt;")
+								.replaceAll(">", "&gt;");
+						mediumxmpData = mediumxmpData.replaceAll("<", "&lt;")
+								.replaceAll(">", "&gt;");
+						smallxmpData = smallxmpData.replaceAll("<", "&lt;")
+								.replaceAll(">", "&gt;");
+
+						out.write("</hr>");
+						out.write("<table border=\"1\">");
+						out.write("<tr><th><b>Thumbnail</b></th><th><b>Embedded XMP Metadata</b></th></tr>");
+						out.write("<tr>");
+						out.write("<td>");
+						out.write("<h3>Large thumbnail: </h3>");
+						out.write("<img src=\"data:image/jpeg;base64,"
+								+ bigbase64 + "\" />");
+						out.write("</td>");
+						out.write("<td>");
+
+						if (bigxmpData != null) {
+							out.write("<pre>");
+							out.write(bigxmpData);
+							out.write("</pre>");
+						} else {
+							out.write("<b>NO METADATA</b>");
+						}
+						out.write("</td>");
+						out.write("</tr>");
+						out.write("<tr>");
+						out.write("<td>");
+						out.write("<h3>Medium thumbnail: </h3>");
+						out.write("<img src=\"data:image/jpeg;base64,"
+								+ smallbase64 + "\" />");
+						out.write("</td>");
+						out.write("<td>");
+						if (mediumxmpData != null) {
+							out.write("<pre>");
+							out.write(mediumxmpData);
+							out.write("</pre>");
+						} else {
+							out.write("<b>NO METADATA</b>");
+						}
+						out.write("</td>");
+						out.write("</tr>");
+						out.write("<tr>");
+						out.write("<td>");
+						out.write("<h3>Tiny thumbnail: </h3>");
+						out.write("<img src=\"data:image/jpeg;base64,"
+								+ tinybase64 + "\" />");
+						out.write("</td>");
+						out.write("<td>");
+						if (smallxmpData != null) {
+							out.write("<pre>");
+							out.write(smallxmpData);
+							out.write("</pre>");
+						} else {
+							out.write("<b>NO METADATA</b>");
+						}
+						out.write("</td>");
+						out.write("</tr>");
+
+						out.write("</table>");
+
+					}
+				}
 			}
-			}
-			
-			
+
 		} catch (StorageEngineException e) {
 			e.printStackTrace();
 		} catch (DatabaseException e) {
 			e.printStackTrace();
 		}
-	
-    }
-    
+
+	}
+
 	/**
 	 * Servlet Initialization method
 	 */
