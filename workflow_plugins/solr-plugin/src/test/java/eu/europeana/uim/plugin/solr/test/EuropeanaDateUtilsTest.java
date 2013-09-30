61beba9eb5826733a4b398586a53cf8f6b196fd1
ymamakis <yorgos.mamakis@kb.nl>
2013-09-30 10:46:59 +0200

    Added complicated cases in EuropeanaDates test

--- workflow_plugins/solr-plugin/src/test/java/eu/europeana/uim/plugin/solr/test/EuropeanaDateUtilsTest.java
+++ workflow_plugins/solr-plugin/src/test/java/eu/europeana/uim/plugin/solr/test/EuropeanaDateUtilsTest.java
@@ -18,35 +18,43 @@ public class EuropeanaDateUtilsTest {
 
 		ProxyType proxy = new ProxyType();
 		List<EuropeanaType.Choice> dates = new ArrayList<EuropeanaType.Choice>();
-		String dcDateString1 = "-150 / -70"; //should give -150 and -70
-		String dcDateString2 = "75/140 AD"; //should give 75 and 140
+		//String dcDateString1 = "-150 / -70"; //should give -150 and -70
+		//String dcDateString2 = "75/140 AD"; //should give 75 and 140
 		String dcDateString3 = "circa 1500 BC"; //should give -1500
-		EuropeanaType.Choice dateChoice1 = new EuropeanaType.Choice();
-		Date date1 = new Date();
-		date1.setString(dcDateString1);
-		dateChoice1.setDate(date1);
-		dates.add(dateChoice1);
-		EuropeanaType.Choice dateChoice2 = new EuropeanaType.Choice();
-		Date date2 = new Date();
-		date2.setString(dcDateString2);
-		dateChoice2.setDate(date2);
-		dates.add(dateChoice2);
+		String dcDateString4 = "1200-1300"; //should be empty
+		String dcDateString5 = "1700";
+//		EuropeanaType.Choice dateChoice1 = new EuropeanaType.Choice();
+//		Date date1 = new Date();
+//		date1.setString(dcDateString1);
+//		dateChoice1.setDate(date1);
+//		dates.add(dateChoice1);
+//		EuropeanaType.Choice dateChoice2 = new EuropeanaType.Choice();
+//		Date date2 = new Date();
+//		date2.setString(dcDateString2);
+//		dateChoice2.setDate(date2);
+//		dates.add(dateChoice2);
 		EuropeanaType.Choice dateChoice3 = new EuropeanaType.Choice();
 		Date date3 = new Date();
 		date3.setString(dcDateString3);
 		dateChoice3.setDate(date3);
 		dates.add(dateChoice3);
+		EuropeanaType.Choice dateChoice4 = new EuropeanaType.Choice();
+		Date date4 = new Date();
+		date4.setString(dcDateString4);
+		dateChoice4.setDate(date4);
+		dates.add(dateChoice4);
+		EuropeanaType.Choice dateChoice5 = new EuropeanaType.Choice();
+		Date date5 = new Date();
+		date5.setString(dcDateString5);
+		dateChoice5.setDate(date5);
+		dates.add(dateChoice5);
 		proxy.setChoiceList(dates);
 		String proplocation = EuropeanaDateUtilsTest.class.getProtectionDomain().getCodeSource().getLocation() + "uimTest.properties";
 		String truncated = proplocation.replace("file:", "");
 		EuropeanaDateUtils.setPath(truncated);
 		List<String> filteredDates = new EuropeanaDateUtils().createEuropeanaYears(proxy);
-		Assert.assertTrue(filteredDates.size()==1);
-		//TEMPORARILY REMOVED
-//		Assert.assertTrue(filteredDates.contains("-150"));
-//		Assert.assertTrue(filteredDates.contains("-70"));
-//		Assert.assertTrue(filteredDates.contains("75"));
-//		Assert.assertTrue(filteredDates.contains("140"));
+		//Assert.assertTrue(filteredDates.size()==1);
+		
 		Assert.assertTrue(filteredDates.contains("-1500"));
 		
 	}
