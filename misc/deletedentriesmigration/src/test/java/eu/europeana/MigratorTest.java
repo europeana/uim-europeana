fce2e35ff0b7b557ca8aff3a444975af0c9d48bd
geomark <georgios.markakis@kb.nl>
2014-01-31 17:28:03 +0100

    Re #1096: Migration Utility

---
+++ misc/deletedentriesmigration/src/test/java/eu/europeana/MigratorTest.java
@@ -0,0 +1,27 @@
+package eu.europeana;
+
+import eu.europeana.uim.storage.StorageEngineException;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public abstract class MigratorTest 
+{
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test 
+    public void testMigration()
+    {
+    	Migrator m = new Migrator();
+    	try {
+			m.populateDeleteStatus();
+		} catch (StorageEngineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
