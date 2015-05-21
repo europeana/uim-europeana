00278cb8ed27635cf0c1d05a4cdec7bf532455e8
ymamakis <yorgos.mamakis@europeana.eu>
2015-05-21 15:59:35 +0200

    Added validation before enriching

--- workflow_plugins/europeana-uim-plugin-enrichment/src/main/java/eu/europeana/uim/enrichment/utils/EnrichmentUtils.java
+++ workflow_plugins/europeana-uim-plugin-enrichment/src/main/java/eu/europeana/uim/enrichment/utils/EnrichmentUtils.java
@@ -9,40 +9,46 @@ import org.apache.solr.common.SolrInputDocument;
 import eu.europeana.enrichment.api.external.InputValue;
 import eu.europeana.uim.enrichment.enums.EnrichmentFields;
 import eu.europeana.uim.enrichment.normalizer.AgentNormalizer;
+import org.apache.commons.validator.routines.UrlValidator;
 
 public class EnrichmentUtils {
-	public List<InputValue> createValuesForEnrichment(
-			SolrInputDocument basicDocument) {
-		List<InputValue> inputValueList = new ArrayList<InputValue>();
-		for (String fieldName : basicDocument.keySet()) {
-			for (EnrichmentFields field : EnrichmentFields.values()) {
-				if (StringUtils.equals(field.getValue(), fieldName)
-						|| StringUtils.startsWith(fieldName, field.getValue())) {
 
-					if (field.equals(EnrichmentFields.DC_CREATOR)
-							|| field.equals(EnrichmentFields.DC_CONTRIBUTOR)) {
-						for (String str : AgentNormalizer
-								.normalize(basicDocument
-										.getFieldValues(fieldName))) {
-							InputValue inValue = new InputValue();
-							inValue.setOriginalField(field.getValue());
-							inValue.setValue(str);
-							inValue.setVocabularies(field.getVocabularies());
-							inputValueList.add(inValue);
-						}
-					} else {
-						for (Object str : basicDocument
-								.getFieldValues(fieldName)) {
-							InputValue inValue = new InputValue();
-							inValue.setOriginalField(field.getValue());
-							inValue.setValue(str.toString());
-							inValue.setVocabularies(field.getVocabularies());
-							inputValueList.add(inValue);
-						}
-					}
-				}
-			}
-		}
-		return inputValueList;
-	}
+    private static UrlValidator validator = new UrlValidator();
+
+    public List<InputValue> createValuesForEnrichment(
+            SolrInputDocument basicDocument) {
+        List<InputValue> inputValueList = new ArrayList<InputValue>();
+        for (String fieldName : basicDocument.keySet()) {
+            for (EnrichmentFields field : EnrichmentFields.values()) {
+                if (!validator.isValid(field.getValue())) {
+                    if (StringUtils.equals(field.getValue(), fieldName)
+                            || StringUtils.startsWith(fieldName, field.getValue())) {
+
+                        if (field.equals(EnrichmentFields.DC_CREATOR)
+                                || field.equals(EnrichmentFields.DC_CONTRIBUTOR)) {
+                            for (String str : AgentNormalizer
+                                    .normalize(basicDocument
+                                            .getFieldValues(fieldName))) {
+                                InputValue inValue = new InputValue();
+                                inValue.setOriginalField(field.getValue());
+                                inValue.setValue(str);
+                                inValue.setVocabularies(field.getVocabularies());
+                                inputValueList.add(inValue);
+                            }
+                        } else {
+                            for (Object str : basicDocument
+                                    .getFieldValues(fieldName)) {
+                                InputValue inValue = new InputValue();
+                                inValue.setOriginalField(field.getValue());
+                                inValue.setValue(str.toString());
+                                inValue.setVocabularies(field.getVocabularies());
+                                inputValueList.add(inValue);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return inputValueList;
+    }
 }
