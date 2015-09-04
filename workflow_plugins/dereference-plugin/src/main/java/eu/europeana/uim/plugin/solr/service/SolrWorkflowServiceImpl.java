eaad78629a2c9289d6183df7738d52c3137087e6
stzanakis <Simon.Tzanakis@theeuropeanlibrary.org>
2015-09-04 16:31:39 +0200

    Fix new mongo authentication

--- workflow_plugins/dereference-plugin/src/main/java/eu/europeana/uim/plugin/solr/service/SolrWorkflowServiceImpl.java
+++ workflow_plugins/dereference-plugin/src/main/java/eu/europeana/uim/plugin/solr/service/SolrWorkflowServiceImpl.java
@@ -9,8 +9,11 @@ import com.google.code.morphia.Morphia;
 import com.google.code.morphia.mapping.DefaultCreator;
 import com.hp.hpl.jena.rdf.model.RDFReaderF;
 import com.hp.hpl.jena.rdf.model.impl.RDFReaderFImpl;
+import com.mongodb.DB;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
 import com.mongodb.ServerAddress;
 
@@ -21,112 +24,117 @@ import eu.europeana.uim.plugin.solr.utils.PropertyReader;
 import eu.europeana.uim.plugin.solr.utils.UimConfigurationProperty;
 
 public class SolrWorkflowServiceImpl implements SolrWorkflowService {
-	private static OsgiExtractor extractor;
-	private static Datastore datastore;
+  private static OsgiExtractor extractor;
+  private static Datastore datastore;
 
-	public SolrWorkflowServiceImpl() {
-		BlockingInitializer datastoreInitializer = new BlockingInitializer() {
+  public SolrWorkflowServiceImpl() {
+    BlockingInitializer datastoreInitializer = new BlockingInitializer() {
 
-			@Override
-			protected void initializeInternal() {
-			
-				if (datastore == null) {
-					Morphia morphia = new Morphia();
-					 morphia.getMapper().getOptions().setObjectFactory(new DefaultCreator() {
-			              @Override
-			              protected ClassLoader getClassLoaderForClass(String clazz, DBObject object) {
-			                  return MongoBundleActivator.getBundleClassLoader();
-			              }
-			       });
-					morphia.map(ControlledVocabularyImpl.class);
-					try {
-					  List<ServerAddress> addresses = new ArrayList<ServerAddress>();
-					  String[] mongoHost = PropertyReader.getProperty(
-					      UimConfigurationProperty.MONGO_HOSTURL).split(",");
-			            for (String mongoStr : mongoHost) {
-			              ServerAddress address = new ServerAddress(mongoStr, 27017);
-			              addresses.add(address);
-			          }
-			          Mongo tgtMongo = new Mongo(addresses);
-						datastore = morphia
-								.createDatastore(
-										tgtMongo,
-										PropertyReader
-												.getProperty(UimConfigurationProperty.MONGO_DB_VOCABULARY));
-					} catch (NumberFormatException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (UnknownHostException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (MongoException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					datastore.ensureIndexes();
-				}
+      @Override
+      protected void initializeInternal() {
 
-			}
-		};
-		datastoreInitializer.initialize(Datastore.class.getClassLoader());
-		
-		final OsgiExtractor extractor = new OsgiExtractor();
-		BlockingInitializer initializer = new BlockingInitializer() {
+        if (datastore == null) {
+          Morphia morphia = new Morphia();
+          morphia.getMapper().getOptions().setObjectFactory(new DefaultCreator() {
+            @Override
+            protected ClassLoader getClassLoaderForClass(String clazz, DBObject object) {
+              return MongoBundleActivator.getBundleClassLoader();
+            }
+          });
+          morphia.map(ControlledVocabularyImpl.class);
+          try {
+            List<ServerAddress> addresses = new ArrayList<ServerAddress>();
+            String[] mongoHost =
+                PropertyReader.getProperty(UimConfigurationProperty.MONGO_HOSTURL).split(",");
+            for (String mongoStr : mongoHost) {
+              ServerAddress address =
+                  new ServerAddress(mongoStr, Integer.parseInt(PropertyReader
+                      .getProperty(UimConfigurationProperty.MONGO_HOSTPORT)));
+              addresses.add(address);
+            }
 
-			@Override
-			protected void initializeInternal() {
-//				if(extractor == null){
-				
-//				}
-				extractor.setDatastore(datastore);
-				
-				
-			}
-		};
-		initializer.initialize(OsgiExtractor.class.getClassLoader());
-		
-		BlockingInitializer vocInitializer = new BlockingInitializer() {
+            List<MongoCredential> credentialsList = new ArrayList<MongoCredential>();
+            MongoCredential credentials =
+                MongoCredential.createCredential(PropertyReader
+                    .getProperty(UimConfigurationProperty.MONGO_USERNAME), PropertyReader
+                    .getProperty(UimConfigurationProperty.MONGO_AUTH_DB), PropertyReader
+                    .getProperty(UimConfigurationProperty.MONGO_PASSWORD).toCharArray());
+            credentialsList.add(credentials);
+            MongoClient client = new MongoClient(addresses, credentialsList);
+            datastore =
+                morphia.createDatastore(client,
+                    PropertyReader.getProperty(UimConfigurationProperty.MONGO_DB_VOCABULARY));
 
-			@Override
-			protected void initializeInternal() {
-				// TODO Auto-generated method stub
-				List<ControlledVocabularyImpl> vocabularies = extractor.getControlledVocabularies();
-				
-			}
-		};
-		vocInitializer.initialize(ControlledVocabularyImpl.class
-				.getClassLoader());
-		
-		BlockingInitializer rdfReaderInitializer = new BlockingInitializer() {
-			
-			@Override
-			protected void initializeInternal() {
-				new RDFReaderFImpl();
-				
-			}
-		};
-		rdfReaderInitializer.initialize(RDFReaderFImpl.class.getClassLoader());
-	}
-	
-	@Override
-	public OsgiExtractor getExtractor() {
-		//OsgiExtractor extractor = new OsgiExtractor(this);
-		if(extractor==null){
-			extractor = new OsgiExtractor(this);
-			extractor.setDatastore(datastore);
-		}
-		
-		return extractor;
-	}
+          } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (MongoException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+          datastore.ensureIndexes();
+        }
 
-	@Override
-	public Datastore getDatastore() {
-		return datastore;
-	}
+      }
+    };
+    datastoreInitializer.initialize(Datastore.class.getClassLoader());
 
-	@Override
-	public RDFReaderF getRDFReaderF() {
-		return new RDFReaderFImpl();
-	}
+    final OsgiExtractor extractor = new OsgiExtractor();
+    BlockingInitializer initializer = new BlockingInitializer() {
+
+      @Override
+      protected void initializeInternal() {
+        // if(extractor == null){
+
+        // }
+        extractor.setDatastore(datastore);
+
+
+      }
+    };
+    initializer.initialize(OsgiExtractor.class.getClassLoader());
+
+    BlockingInitializer vocInitializer = new BlockingInitializer() {
+
+      @Override
+      protected void initializeInternal() {
+        // TODO Auto-generated method stub
+        List<ControlledVocabularyImpl> vocabularies = extractor.getControlledVocabularies();
+
+      }
+    };
+    vocInitializer.initialize(ControlledVocabularyImpl.class.getClassLoader());
+
+    BlockingInitializer rdfReaderInitializer = new BlockingInitializer() {
+
+      @Override
+      protected void initializeInternal() {
+        new RDFReaderFImpl();
+
+      }
+    };
+    rdfReaderInitializer.initialize(RDFReaderFImpl.class.getClassLoader());
+  }
+
+  @Override
+  public OsgiExtractor getExtractor() {
+    // OsgiExtractor extractor = new OsgiExtractor(this);
+    if (extractor == null) {
+      extractor = new OsgiExtractor(this);
+      extractor.setDatastore(datastore);
+    }
+
+    return extractor;
+  }
+
+  @Override
+  public Datastore getDatastore() {
+    return datastore;
+  }
+
+  @Override
+  public RDFReaderF getRDFReaderF() {
+    return new RDFReaderFImpl();
+  }
 
 }
--- workflow_plugins/dereference-plugin/src/main/java/eu/europeana/uim/plugin/solr/utils/UimConfigurationProperty.java
+++ workflow_plugins/dereference-plugin/src/main/java/eu/europeana/uim/plugin/solr/utils/UimConfigurationProperty.java
@@ -10,6 +10,9 @@ public enum UimConfigurationProperty {
 	MONGO_HOSTURL("mongo.hostUrl"),
 	MONGO_HOSTPORT("mongo.hostPort"),
 	MONGO_DB_VOCABULARY("mongo.db.vocabulary"),
+	MONGO_AUTH_DB("mongo.auth.db"),
+	MONGO_USERNAME("mongo.username"),
+	MONGO_PASSWORD("mongo.password"),
 	UIM_BCLIST_PATH("uim.bclist.path");
 	
 	String field;
